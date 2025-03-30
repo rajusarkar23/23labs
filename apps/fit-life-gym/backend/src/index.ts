2e9107939885d3c8a287f2cb364a1d0ae13e585f
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-03-30 17:08:30 +0530

    fit-life-lab/backend: initial express app setup

--- apps/fit-life-gym/backend/src/index.ts
+++ apps/fit-life-gym/backend/src/index.ts
@@ -1,3 +1,19 @@
+import "dotenv/config"
 import express from "express";
+import cookieParser from "cookie-parser"
 
 const app = express();
+const PORT = process.env.PORT || 8888
+
+// allow cookie
+app.use(cookieParser())
+// alloe json
+app.use(express.json())
+
+app.listen(PORT, () => {
+    console.log(`Fit life gym's backend is up and runnign on port ${PORT}`);
+})
+
+app.get("/", (req, res) => {
+    res.send("Hello there!!, via backend serevr.")
+})
