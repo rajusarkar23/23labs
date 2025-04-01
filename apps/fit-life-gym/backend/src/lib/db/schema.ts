16d9da57ce9b798fc14bedb9ea9ebc5afdd98214
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-04-01 14:29:17 +0530

    add posts schema

--- apps/fit-life-gym/backend/src/lib/db/schema.ts
+++ apps/fit-life-gym/backend/src/lib/db/schema.ts
@@ -1,8 +1,9 @@
-import { boolean, integer, pgEnum, pgTable, text } from "drizzle-orm/pg-core";
+import { boolean, integer, pgEnum, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { profileImage } from "../../config";
 
 export const genderEnum = pgEnum("gender", ["male", "female"])
 
+// members table
 export const member = pgTable("members",{
     id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
     name: text("name").notNull(),
@@ -15,4 +16,21 @@ export const member = pgTable("members",{
     profession: text("profession"),
     gender: genderEnum("gender"),
     dob: text("dob"),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().$onUpdate(() => new Date())
 })
+
+// posts table
+export const post = pgTable("posts", {
+    id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+    textContent: text("text_content").notNull(),
+    postImageUrl: text("post_image_url"),
+    postBelongTo: integer("post_belong_to")
+      .notNull()
+      .references(() => member.id, { onDelete: "cascade" }),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at  ")
+      .notNull()
+      .$onUpdate(() => new Date()),
+  });
+  
