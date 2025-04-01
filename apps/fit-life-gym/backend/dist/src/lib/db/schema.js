b379a7f42c47ca025ed57b4bd91470169d6849a9
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-04-01 13:56:21 +0530

    members schema update

--- apps/fit-life-gym/backend/dist/src/lib/db/schema.js
+++ apps/fit-life-gym/backend/dist/src/lib/db/schema.js
@@ -5,15 +5,15 @@ const pg_core_1 = require("drizzle-orm/pg-core");
 const config_1 = require("../../config");
 exports.genderEnum = (0, pg_core_1.pgEnum)("gender", ["male", "female"]);
 exports.member = (0, pg_core_1.pgTable)("members", {
-    id: (0, pg_core_1.integer)().primaryKey().generatedAlwaysAsIdentity(),
-    name: (0, pg_core_1.text)().notNull(),
-    userName: (0, pg_core_1.text)().notNull(),
-    email: (0, pg_core_1.text)().notNull(),
-    password: (0, pg_core_1.text)().notNull(),
-    otp: (0, pg_core_1.text)().notNull(),
-    isAccountVerified: (0, pg_core_1.boolean)().default(false),
-    profileImage: (0, pg_core_1.text)().notNull().default(`${config_1.profileImage}`),
-    profession: (0, pg_core_1.text)(),
-    gender: (0, exports.genderEnum)(),
-    dob: (0, pg_core_1.text)(),
+    id: (0, pg_core_1.integer)("id").primaryKey().generatedAlwaysAsIdentity(),
+    name: (0, pg_core_1.text)("name").notNull(),
+    userName: (0, pg_core_1.text)("user_name").notNull(),
+    email: (0, pg_core_1.text)("email").notNull(),
+    password: (0, pg_core_1.text)("password").notNull(),
+    otp: (0, pg_core_1.text)("otp").notNull(),
+    isAccountVerified: (0, pg_core_1.boolean)("is_account_verified").default(false),
+    profileImage: (0, pg_core_1.text)("profile_image").notNull().default(`${config_1.profileImage}`),
+    profession: (0, pg_core_1.text)("profession"),
+    gender: (0, exports.genderEnum)("gender"),
+    dob: (0, pg_core_1.text)("dob"),
 });
--- apps/fit-life-gym/backend/src/lib/db/schema.ts
+++ apps/fit-life-gym/backend/src/lib/db/schema.ts
@@ -4,15 +4,15 @@ import { profileImage } from "../../config";
 export const genderEnum = pgEnum("gender", ["male", "female"])
 
 export const member = pgTable("members",{
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    name: text().notNull(),
-    userName: text().notNull(),
-    email: text().notNull(),
-    password: text().notNull(),
-    otp: text().notNull(),
-    isAccountVerified: boolean().default(false),
-    profileImage: text().notNull().default(`${profileImage}`),
-    profession: text(),
-    gender: genderEnum(),
-    dob: text(),
+    id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+    name: text("name").notNull(),
+    userName: text("user_name").notNull(),
+    email: text("email").notNull(),
+    password: text("password").notNull(),
+    otp: text("otp").notNull(),
+    isAccountVerified: boolean("is_account_verified").default(false),
+    profileImage: text("profile_image").notNull().default(`${profileImage}`),
+    profession: text("profession"),
+    gender: genderEnum("gender"),
+    dob: text("dob"),
 })
