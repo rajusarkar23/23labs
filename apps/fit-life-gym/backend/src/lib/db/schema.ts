0b5da0e0fbf1e59d84f50013c0fd80038d0ac28e
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-04-10 14:54:17 +0530

    add admin table

--- apps/fit-life-gym/backend/src/lib/db/schema.ts
+++ apps/fit-life-gym/backend/src/lib/db/schema.ts
@@ -23,6 +23,20 @@ export const member = pgTable("members",{
     updatedAt: timestamp("updated_at").notNull().$onUpdate(() => new Date())
 })
 
+// admin table
+export const admin = pgTable("admins",{
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  name: text("name").notNull(),
+  userName: text("user_name").notNull(),
+  email: text("email").notNull(),
+  password: text("password").notNull(),
+  otp: text("otp").notNull(),
+  isAccountVerified: boolean("is_account_verified").default(false),
+  profileImage: text("profile_image").notNull().default(`${profileImage}`),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().$onUpdate(() => new Date())
+})
+
 // posts table
 export const post = pgTable("posts", {
     id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
